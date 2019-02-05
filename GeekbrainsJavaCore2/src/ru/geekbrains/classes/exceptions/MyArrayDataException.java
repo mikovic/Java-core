787f8da07c85f0311a9d6adceda5a689def1da90
Konstantin Minakov <mikovic@yandex.ru>
2019-02-05 13:16:37 +0400

    Переделка ДЗ№2 Без обработки пользователского ввода

--- GeekbrainsJavaCore2/src/ru/geekbrains/classes/exceptions/MyArrayDataException.java
+++ GeekbrainsJavaCore2/src/ru/geekbrains/classes/exceptions/MyArrayDataException.java
@@ -2,15 +2,22 @@ package ru.geekbrains.classes.exceptions;
 
 public class MyArrayDataException extends Exception {
     private int i;
-    private  int k;
-    public MyArrayDataException (int i, int k) {
+    private int k;
+    private String value;
+
+    public MyArrayDataException (int i, int k, String value) {
+        super("Ошибка в приведении! строка "+i+" столбец "+k+". Значение: " +value);
         this.i = i;
         this.k = k;
+        this.value = value;
+
     }
 
-    public void getExceptionMsg(){
-        System.out.println("Ошибка в приведении! строка "+i+" столбец "+k);
+    public int getK() {
+        return k;
     }
 
-
+    public int getI() {
+        return i;
+    }
 }
--- GeekbrainsJavaCore2/src/ru/geekbrains/classes/exceptions/MyArraySizeException.java
+++ GeekbrainsJavaCore2/src/ru/geekbrains/classes/exceptions/MyArraySizeException.java
@@ -2,10 +2,8 @@ package ru.geekbrains.classes.exceptions;
 
 public class MyArraySizeException extends Exception  {
     public MyArraySizeException(){
+        super("Ошибка с размером массива");
+    }
 
-    }
-    public void getExceptionMsg (){
-        System.out.println("Ошибка с размером массива");
-    }
 
 }
